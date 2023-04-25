7c9529954432f451c3a33239e8c52880c984e7c2
tsunedzune <130538911+tsunedzune@users.noreply.github.com>
2023-04-25 23:40:16 +0200

    eclipse still broken (sadly)

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -2,6 +2,9 @@ package idh.java;
 
 
 public class Main {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		
@@ -9,7 +12,11 @@ public class Main {
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
 
-         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne und noch eine Zahnbürste und Gaffel Wiess und eine Kamera und eine Sonnenbrille und ein Unterhaltungselektroniktelefonverarbeitungspartner und Lichtschutzfaktor 50.");
+<<<<<<< HEAD
+         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und eine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne und noch eine Zahnbürste und Gaffel Wiess und eine Kamera und eine Sonnenbrille und ein Unterhaltungselektroniktelefonverarbeitungspartner und Lichtschutzfaktor 50 und einen Volleyball.");
+=======
+         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne und noch eine Zahnbürste und Gaffel Wiess und eine Kamera und eine Sonnenbrille und ein Unterhaltungselektroniktelefonverarbeitungspartner und Lichtschutzfaktor 50.");;
+>>>>>>> c0e966c7c39b310e03d4f8fb4fe56b4d537bde83
 //github.com/idh-cologne-java-2-summer-2023/exercise-03.git
 	};
 }

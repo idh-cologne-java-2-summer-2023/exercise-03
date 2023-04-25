034cdc4e73eb2b73197dd2a375b95e1b79f026cd
BerivanAlt <berivan.altunsaray@web.de>
2023-04-25 22:16:38 +0200

    Lichtschutzfaktor 50

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,11 +6,7 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-<<<<<<< HEAD
-		System.out.println("Ich packe in meinen Koffer Lichtschutzfaktor 50.");
-=======
-
-         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne und noch eine Zahnbürste und Gaffel Wiess und eine Kamera.");
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2023/exercise-03.git
+ System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne und noch eine Zahnbürste und Gaffel Wiess und eine Kamera und einen Lichtschutzfaktor 50.");
+//github.com/idh-cologne-java-2-summer-2023/exercise-03.git
 	};
 }
